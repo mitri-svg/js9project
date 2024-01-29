25b7d5ad0afbae25c95ea22bc72316f16392c80d
Mitri <hmirseid54@gmail.com>
2024-01-29 23:09:49 +0400

    project yarandi ve qurtardi

---
+++ script.js
@@ -0,0 +1,17 @@
+let suret;
+suret = +prompt('Siz nece km/saat sureti ile surmusunuz?');
+let netice;
+
+if(suret > 120) {
+    netice = ' 300 AZN cerime alinir';
+} else if(suret > 100) {
+    netice = ' 100 AZN cerime alinir';
+} else if(suret > 80) {
+    netice = ' 40 AZN cerime alinir';
+} else if(suret > 60) {
+    netice = ' 20 AZN cerime alinir';
+} else{
+    netice = ' cerime goturulmur ';
+}
+
+document.getElementById('suret').innerHTML = 'Siz ' + suret + ' km/saat sureti ile getdiyivize gore sizden ' + netice + ' .'
